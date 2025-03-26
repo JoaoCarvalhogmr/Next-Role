b0283a10e076f13e9ced5c9b7e9dd37729394fd6
João Carvalho <joaocarvalhoum@gmail.com>
2025-03-26 08:56:39 +0000

    Add database setup with configuration

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -19,12 +19,15 @@ const CreateJobForm = () => {
           mode: JobMode.FullTime
         },
       })
+
+      const {reset} = form;
       
     const onSubmit = (values: CreateAndEditJobType) => {
         console.log(values)
+        reset();
     }
-
     return (
+
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
@@ -32,21 +35,16 @@ const CreateJobForm = () => {
         >
           <h2 className='capitalize font-semibold text-4xl mb-6'>add job</h2>
           <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3 items-start'>
-            {/* position */}
+            
             <CustomFormField name='position' control={form.control} />
-            {/* company */}
             <CustomFormField name='company' control={form.control} />
-            {/* location */}
             <CustomFormField name='location' control={form.control} />
-  
-            {/* job status */}
             <CustomFormSelect
               name='status'
               control={form.control}
               labelText='job status'
               items={Object.values(JobStatus)}
             />
-            {/* job  type */}
             <CustomFormSelect
               name='mode'
               control={form.control}
